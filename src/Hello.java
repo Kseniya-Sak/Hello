486d25d6caef0f7f68d06739b4ee3aad3bfef8ea
KseniyaSakovich <ya.cak94@gmail.com>
2022-10-05 16:34:49 +0300

    change file Hello

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Hello {
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(1);
+        System.out.println("www");
+    }
 }
